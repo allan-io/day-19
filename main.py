b5ac0abe3fa1b8a7b02977c955fe0efadc40e692
Allan Oliveira <allanoliveira.dev@gmail.com>
2024-10-13 01:17:43 -0300

    Create Food class. Make food appear in random places in screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 from turtle import Screen
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,7 +10,13 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
-print("hi")
+food = Food()
+
+screen.listen()
+screen.onkey(fun=snake.up, key="Up")
+screen.onkey(fun=snake.down, key="Down")
+screen.onkey(fun=snake.left, key="Left")
+screen.onkey(fun=snake.right, key="Right")
 
 game_is_on = True
 while game_is_on:
@@ -17,12 +24,8 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-    screen.listen()
-    screen.onkey(fun=snake.up, key="Up")
-    screen.onkey(fun=snake.down, key="Down")
-    screen.onkey(fun=snake.left, key="Left")
-    screen.onkey(fun=snake.right, key="Right")
-
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 screen.exitonclick()
